44447ebf345e1e9b9169758bd14fe44fe0e10aec
Neeschal1 <neeschalpok04@gmail.com>
2025-12-28 20:43:29 +0545

    pdf embeddings modified

--- paperaibackend/apps/ai/serializers.py
+++ paperaibackend/apps/ai/serializers.py
@@ -24,52 +24,36 @@ class PDFModelSerializers(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        title = validated_data["Title"]
-        url = validated_data["URL"]
-
+        title = validated_data['Title']
+        url = validated_data['URL']
+        
         response = requests.get(url)
         response.raise_for_status()
-
-        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+        
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.pdf')
         temp_file.write(response.content)
         temp_file.close()
-
-        embedding = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2"
-        )
-
-        pdf_text = ""
-        data = []
-
+        
+        embedding = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+        pdf_text = ''
+        
         try:
             loader = PyPDFLoader(temp_file.name)
-            docs = loader.load()  # Makes every page of the pdf as a single document
-            for page in docs:
-                # Holds the texts of each pages inside a page_content
-                page_content = page.page_content
-
-                # Split page into sentences using regex (handles '.', '?', '!', and newlines)
-                sentences = [
-                    s.strip() for s in re.split(r"[.?!\n,]", page_content) if s.strip()
-                ]
-
-            pdf_text = "\n".join([doc.page_content for doc in docs])
-
-            flat_data = [sentence for page in data for sentence in page]
-            embedded_pdf = embedding.embed_documents(flat_data)
-
+            docs = loader.load()
+            pdf_text = '\n'.join([doc.page_content for doc in docs])
+            embedded_pdf = embedding.embed_query(pdf_text)
         finally:
             os.remove(temp_file.name)
-
+        
         pdf_detail = PDFModel.objects.create(
-            Title=title,
-            URL=url,
-            Plain_contents=pdf_text,
-            Embedded_contents=embedded_pdf,
+            Title = title,
+            URL = url,
+            Plain_contents = pdf_text,
+            Embedded_contents = embedded_pdf
         )
-
-        return pdf_detail
-
+        
+        return  pdf_detail
+    
 
 class CommunicationSerializers(serializers.ModelSerializer):
     class Meta:
